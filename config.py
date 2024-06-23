3d57e153191ea8d2b8c846bb7535a5b9cc4b39f8
Husam Alshehadat <halshehadat@proton.me>
2024-06-23 09:27:20 -0700

    config file

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    API_KEY = os.environ.get('API_KEY') or 'your-api-key'
-    DEBUG = os.environ.get('FLASK_DEBUG', 'False').lower() in ('true', '1', 't')
--- webserver.py
+++ webserver.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 import subprocess
 import action_grippers.py as actions
+import argparse
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='web/static', template_folder='web')
+
+
 
 @app.route('/rotate', methods=['POST'])
 def rotate():
@@ -57,8 +60,46 @@ def gripper():
         else:
             return jsonify({'error': 'Gripper action or force not provided in the request'}), 400
 
+def get_utterance():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        try:
+            # Adjust for ambient noise and set a higher threshold
+            print("Adjusting for ambient noise. Please wait...")
+            r.adjust_for_ambient_noise(source, duration=3)
+            print("Say something!")
+            audio = r.listen(source, timeout=60, phrase_time_limit=5)
+            print("Processing...")
+            # Simulate processing here
+        except Exception as e:
+            print(f"Error: {str(e)}")  # Customize or handle logging
+            return "Device not connected or audio setup issue."
+
+@app.route('/start-recording', methods=['POST'])
+def start_recording():
+    result = get_utterance()
+    if "Error" in result:
+        return jsonify({'status': 'error', 'message': result}), 500
+    return jsonify({'status': 'success', 'message': 'Recording started successfully', 'transcript': result}), 200
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # Set up command line arguments
+    parser = argparse.ArgumentParser(description="Start the Flask web server for the Robotic Arm Controller.")
+    parser.add_argument('-W', '--web', action='store_true', help='Start the web server')
+    
+    args = parser.parse_args()
 
-
+    if args.web:
+        port = 5000
+        os.system('clear')
+        print("\n===============================================================")
+        print("  Robotic Arm Controller Server is running...")
+        print(f"  Please open your browser and visit http://localhost:5000/:{port}")
+        print("===============================================================\n")
+        print("################## Press Ctrl + C to exit #####################\n")
+        
+        app.run(debug=True, host='0.0.0.0', port=port)
+    else:
+        print("Web server not started. Use '--web' or '-W' to start the server.")
 
